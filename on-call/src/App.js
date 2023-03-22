8643188fab5de15bde322f7e5c35ca85294d6e33
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-22 21:03:25 +0000

    changed tailwind.config.js file to allow tailwindcss to render

--- on-call/src/App.js
+++ on-call/src/App.js
@@ -1,8 +1,10 @@
 import PlayAudio from "./components/PlayAudio";
+import './index.css'
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline"> Hello world its me your main man yaxxy</h1>
     <PlayAudio />
     </div>
   );
