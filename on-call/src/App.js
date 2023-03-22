4c0d75d0d6083e1d584cf9c6e80dbe6627f10233
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-22 21:30:42 +0000

    created nav container

--- on-call/src/App.js
+++ on-call/src/App.js
@@ -4,7 +4,8 @@ import SideNav from './components/SideNav';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App border-4 border-rose-500 ... min-h-full">
+      <h1> ON CALL</h1>
       <SideNav />
     </div>
   );
--- on-call/src/components/SideNav.js
+++ on-call/src/components/SideNav.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const SideNav = () => {
   return (
-    <div className='float-left bg-[#000] w-1/4 h-full'></div>
+    <div className='float-left bg-[#000]'>
+      <h1 className='text-white'>HELLO</h1>
+    </div>
   )
 }
 
