decd4f8f99181ee38a2a6e8f032e340878093632
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-23 20:26:22 +0000

    added h1 and menu items to nav

--- on-call/src/components/SideNav.js
+++ on-call/src/components/SideNav.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const SideNav = () => {
   return (
-    <div className="w-1/6 bg-[#fff] hover:h-full flex flex-col mb-6 mb-10">
-      <h1 className="text-center text-2xl mb-20 mt-5 text-dec"> ON CALL</h1>
-      <div className="bg-[grey] w-100 h-10 mb-10 pr-10 text-right">Home</div>
-      <div className="bg-[grey] w-100 h-10  mb-10 pr-10 text-right"> About us</div>
-      <div className="bg-[grey] w-100 h-10 mb-10 pr-10 text-right"> Forms</div>
-      <div className="bg-[grey] w-100 h-10 mb-10 pr-10 text-right">Pranks</div>
-      <div className="bg-[grey] w-100 h-10 mb-10 pr-10 text-right">Texts</div>
-      <div className="bg-[grey] w-100 h-10 mb-10 pr-10 text-right">Contact</div>
-      <div className="bg-[grey] w-100 h-10  mb-10 pr-10 text-right">Information</div>
+    <div className="w-1/5 h-full bg-[grey] flex flex-col mb-6 mb-10">
+      <h1 className="text-center text-4xl mb-20 mt-5 text-dec"> Meals</h1>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Pizzas</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Burgers</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Chicken</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Wraps</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Sandwiches</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Drinks</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Deserts</div>
+      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl">Drinks</div>
+
     </div>
   )
 }
