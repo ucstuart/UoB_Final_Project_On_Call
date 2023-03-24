8cea7f72f7f707a38448a1411765ff7b001c63dc
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-24 20:53:58 +0000

    added ADD button to menu item

--- on-call/src/components/meal-components/PizzaItem.js
+++ on-call/src/components/meal-components/PizzaItem.js
@@ -4,11 +4,19 @@ import { Link } from "react-router-dom";
 const PizzaItem = () => {
   return (
     <div className="w-1/5 h-full bg-[#fff] flex flex-col mb-10">
-      <h1 className="text-5xl">Pizza</h1>
-      <h3 className="text-xl">Pizza</h3>
+      <h1 className="text-5xl mb-2">Pizza</h1>
+      <h3 className="text-l mb-5">Pizza</h3>
+      <div className="flex justify-between ">
+        <h3 className="mb-1">Pepperoni Feast</h3>
+        <p className="">£10.00</p> <button>ADD</button>
+      </div>
+
+      <div>
+        <p> Double Pepperoni, Extra Mozzarella Cheese</p>
+      </div>
       <Link to="/">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-          Home
+          Back
         </button>
       </Link>
     </div>
