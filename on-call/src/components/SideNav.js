37b1fb3a1008500aefe3a6d06ecd18f326d396a0
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-24 19:43:01 +0000

    added react router links to all menu item components

--- on-call/src/components/SideNav.js
+++ on-call/src/components/SideNav.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 const SideNav = () => {
   return (
-    <div className="w-1/5 h-full bg-[grey] flex flex-col mb-6 mb-10">
+    <div className="w-1/5 h-full bg-[#fff] flex flex-col ml-10 mb-10">
       <h1 className="text-center text-4xl mb-20 mt-5 text-dec">Meals</h1>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"><Link to='/Pizzaitem'>Pizzas</Link></div>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"> <Link>Burgers</Link></div>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"> <Link>Chicken</Link></div>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"> <Link>Wraps</Link></div>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"> <Link>Sandwiches</Link></div>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"> <Link>Deserts</Link></div>
-      <div className="bg-[grey] w-100 h-10 mb-10 text-2xl"> <Link>Drinks</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='/Pizzaitem'>Pizzas</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='/Burgers'>Burgers</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='Chicken'>Chicken</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='Wraps'>Wraps</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='Sandwiches'>Sandwiches</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='Deserts'>Deserts</Link></div>
+      <div className=" w-100 h-10 mb-10 text-2xl"><Link to='Drinks'>Drinks</Link></div>
     </div>
   );
 };
