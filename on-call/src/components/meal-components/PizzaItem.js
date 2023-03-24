c295db7f47bd7cbf5fe635856e0e0858db1372cd
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-24 21:02:54 +0000

    added outer div to menu-item

--- on-call/src/components/meal-components/PizzaItem.js
+++ on-call/src/components/meal-components/PizzaItem.js
@@ -6,13 +6,17 @@ const PizzaItem = () => {
     <div className="w-1/5 h-full bg-[#fff] flex flex-col mb-10">
       <h1 className="text-5xl mb-2">Pizza</h1>
       <h3 className="text-l mb-5">Pizza</h3>
-      <div className="flex justify-between ">
-        <h3 className="mb-1">Pepperoni Feast</h3>
-        <p className="">£10.00</p> <button>ADD</button>
-      </div>
-
-      <div>
-        <p> Double Pepperoni, Extra Mozzarella Cheese</p>
+      <div className="mb-10">
+        <div className="flex justify-between">
+          <h3 className="mb-1">Pepperoni Feast</h3>
+          <p className="">£10.00</p>
+          <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
+            ADD
+          </button>
+        </div>
+        <div>
+          <p> Double Pepperoni, Extra Mozzarella Cheese</p>
+        </div>
       </div>
       <Link to="/">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
