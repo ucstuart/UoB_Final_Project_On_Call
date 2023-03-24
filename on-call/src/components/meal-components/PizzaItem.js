f8d8079a6047e02b2313ceef9b0078e33a2e13d0
adam willetts <adam-willetts@hotmail.co.uk>
2023-03-24 21:31:49 +0000

    added all pizza items to pizza component

--- on-call/src/components/meal-components/PizzaItem.js
+++ on-call/src/components/meal-components/PizzaItem.js
@@ -4,70 +4,68 @@ import { Link } from "react-router-dom";
 const PizzaItem = () => {
   return (
     <div className="w-1/5 h-full bg-[#fff] flex flex-col mb-10">
-      <h1 className="text-5xl mb-2">Pizza</h1>
-      <h3 className="text-l mb-5">Pizza</h3>
+      <h1 className="text-5xl mb-10 mt-5">Pizza</h1>
+
       <div className="mb-10">
         <div className="flex justify-between">
-          <h3 className="mb-1">Pepperoni Feast</h3>
-          <p className="">£10.00</p>
+          <h3 className="mb-1 text-xl">Marghertia</h3>
+          <p className="ml-auto mr-5">£8.00</p>
           <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
             ADD
           </button>
         </div>
         <div>
-          <p> Double Pepperoni, Extra Mozzarella Cheese</p>
+          <p className="text-sm"> Mozzarella Cheese, Tomato Sauce</p>
+        </div>
+      </div>
+<div className="mb-10">
+        <div className="flex justify-between">
+          <h3 className="mb-1 text-xl">Pepperoni Feast</h3>
+          <p className="ml-auto mr-5">£10.00</p>
+          <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
+            ADD
+          </button>
+        </div>
+        <div>
+          <p className="text-sm"> Double Pepperoni, Extra Mozzarella Cheese</p>
+        </div>
+      </div>
+      <div className="mb-10">
+        <div className="flex justify-between">
+          <h3 className="mb-1 text-xl">Meat Feast</h3>
+          <p className="ml-auto mr-5">£12.00</p>
+          <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
+            ADD
+          </button>
+        </div>
+        <div>
+          <p className="text-sm">Spicy Pork, Ham, Pepperoni, Seasoned Minced Beef</p>
         </div>
       </div>
 
       <div className="mb-10">
         <div className="flex justify-between">
-          <h3 className="mb-1">Marghertia</h3>
-          <p className="">£8.00</p>
+          <h3 className="mb-1 text-xl">The Meaty One</h3>
+          <p className="ml-auto mr-5">£15.00</p>
           <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
             ADD
           </button>
         </div>
         <div>
-          <p> Mozzarella Cheese, Tomato Sauce</p>
+          <p className="text-sm">Pepperoni, Spicy Pork, Seasoned Minced Beef, Smoked Bacon, Chicken</p>
         </div>
       </div>
 
       <div className="mb-10">
         <div className="flex justify-between">
-          <h3 className="mb-1">Meat Feast</h3>
-          <p className="">£12.00</p>
+          <h3 className="mb-1 text-xl">Beef Sizzler</h3>
+          <p className="ml-auto mr-5">£18.00</p>
           <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
             ADD
           </button>
         </div>
         <div>
-          <p>Spicy Pork, Ham, Pepperoni, Seasoned Minced Beef</p>
-        </div>
-      </div>
-
-      <div className="mb-10">
-        <div className="flex justify-between">
-          <h3 className="mb-1">The Meaty One</h3>
-          <p className="">£15.00</p>
-          <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
-            ADD
-          </button>
-        </div>
-        <div>
-          <p>Pepperoni, Spicy Pork, Seasoned Minced Beef, Smoked Bacon, Chicken</p>
-        </div>
-      </div>
-
-      <div className="mb-10">
-        <div className="flex justify-between">
-          <h3 className="mb-1">Beef Sizzler</h3>
-          <p className="">£18.00</p>
-          <button className="border-2 border-violet-800 px-3 mr-2 text-sm">
-            ADD
-          </button>
-        </div>
-        <div>
-          <p>Green Chillies, Jalapenos, Red Onions, Roquito Peppers, Seasoned Minced Beef</p>
+          <p className="text-sm">Green Chillies, Jalapenos, Red Onions, Roquito Peppers, Seasoned Minced Beef</p>
         </div>
       </div>
       <Link to="/">
