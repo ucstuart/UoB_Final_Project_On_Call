14a667c5cc468fec09ea693c41ec8813925d0597
Marilena-C <marilena.cancel@gmail.com>
2023-03-31 18:56:50 +0100

    navbar

--- on-call/src/App.js
+++ on-call/src/App.js
@@ -1,23 +1,33 @@
 import "./index.css";
 import "./index";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import {Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Review from "./components/Review";
+import Navbar from "./components/Navbar";
 import SecondNav from "./components/SecondNav";
 import Hero from "./components/Hero";
+import ContactUs from "./components/ContactUs";
+import CheckOut from "./components/CheckOut";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-    <div className="app-container">
-   
+     <div className="app-container">
+
+      <Navbar />
       <SecondNav />
-    
+
       <Routes>
-        <Route path="/" element={<Hero/> } />
         <Route path="Home" element={<Home />} />
-        <Route path="Review" element={ <Review /> } />
+        <Route path="/" element={<Hero />} />
+        <Route path="ContactUs" element={<ContactUs />} />
+        <Route path="Review" element={<Review />} />
+        <Route path="CheckOut" element={<CheckOut />} />
+        <Route path="SignIn" element={<SignIn />} />
       </Routes>
     </div>
+ 
   );
 }
 
